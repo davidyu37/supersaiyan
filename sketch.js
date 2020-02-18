b350533cd0b8462da5163aeddc17f6e2733e36d6
David <davidyu37@aliyun.com>
2020-02-18 20:15:22 +0800

    sound effect added

--- sketch.js
+++ sketch.js
@@ -4,13 +4,64 @@ const videoHeight = window.screen.height;
 let video;
 let poseNet;
 let poses = [];
+let state = "normal";
+let powerLevel;
+let currentCharge = 2;
+let transformComplete = false;
 
 let super_saiyan_hair, goku_hair, aurora;
+let explodeSound, chargeSound;
+
+class PowerLevel {
+  constructor(initialPowerLevel) {
+    this.level = initialPowerLevel;
+    this.super_saiyan_level = 150000000;
+  }
+
+  isSuperSaiyan() {
+    if (this.level >= this.super_saiyan_level) {
+      return true;
+    }
+    return false;
+  }
+
+  updatePowerLevel(addValue) {
+    this.level += addValue;
+  }
+
+  show() {
+    const processNumber = Math.floor(this.level);
+    const addedComma = processNumber
+      .toString()
+      .replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+
+    const showText = `Power Level: ${addedComma}`;
+    const fontSize = 60;
+    const tWidth = textWidth(showText);
+    const startX = 100;
+    const startY = 100;
+    noStroke();
+    fill(4, 228, 147, 200);
+    rect(startX - 20, startY - 80, tWidth + 40, fontSize * 2, 20);
+    fill(247, 255, 177);
+    textSize(fontSize);
+    text(showText, startX, startY);
+  }
+}
+
+const custom_model = {
+  model: "../models/model_v2.json",
+  metadata: "../models/model_meta_v2.json",
+  weights: "../models/model.weights_v2.bin"
+};
 
 function preload() {
   super_saiyan_hair = loadImage("assets/hair.png");
   goku_hair = loadImage("assets/goku-hair.png");
   aurora = loadImage("assets/aurora.png");
+  soundFormats("mp3", "ogg");
+  explodeSound = loadSound("assets/charge-explode.mp3");
+  chargeSound = loadSound("assets/charging.mp3");
 }
 
 function setup() {
@@ -20,28 +71,139 @@ function setup() {
 
   // Create a new poseNet method with a single detection
   poseNet = ml5.poseNet(video, modelReady);
-  // This sets up an event that fills the global variable "poses"
-  // with an array every time new poses are detected
+
+  loadBrain();
+
   poseNet.on("pose", function(results) {
     poses = results;
+    gotPoses(poses);
   });
   // Hide the video element, and just show the canvas
   video.hide();
 }
 
 function modelReady() {
-  select("#status").html("Model Loaded");
+  console.log("model loaded");
+}
+
+function gotPoses(poses) {
+  if (poses.length > 0) {
+    pose = poses[0].pose;
+    skeleton = poses[0].skeleton;
+    if (pose) {
+      classifyPose(pose);
+    }
+  }
+}
+
+function loadBrain() {
+  let options = {
+    inputs: 34,
+    outputs: 2,
+    task: "classification",
+    debug: true
+  };
+  brain = ml5.neuralNetwork(options);
+
+  brain.load(custom_model, brainLoaded);
+}
+
+function brainLoaded() {
+  console.log("pose classification ready!");
+  powerLevel = new PowerLevel(9000);
+}
+
+function classifyPose(pose) {
+  if (pose) {
+    let inputs = [];
+    for (let i = 0; i < pose.keypoints.length; i++) {
+      let x = pose.keypoints[i].position.x;
+      let y = pose.keypoints[i].position.y;
+      inputs.push(x);
+      inputs.push(y);
+    }
+    brain.classify(inputs, gotResult);
+  }
+}
+
+function gotResult(error, results) {
+  if (results[0] && results[1]) {
+    const normal = results[0].label === "normal" ? results[0] : results[1];
+    const constipated =
+      results[0].label === "constipated" ? results[0] : results[1];
+    if (constipated.confidence > 0.9) {
+      if (state !== "charging") {
+        console.log("activate charge");
+        explodeSound.play();
+        chargeSound.stop();
+        chargeSound.loop();
+      }
+
+      if (checkSupersaiyan()) {
+        if (!transformComplete) {
+          explodeSound.play();
+          transformComplete = true;
+        }
+      }
+      state = "charging";
+      return;
+    } else {
+      if (state !== "normal") {
+        if (checkSupersaiyan()) {
+          chargeSound.stop();
+          chargeSound.loop();
+        } else {
+          chargeSound.stop();
+        }
+      }
+      state = "normal";
+    }
+  }
 }
 
 function draw() {
   image(video, 0, 0, width, height);
-  drawSuperSaiyanAurora();
-  drawSuperSaiyanHair();
+
+  // console.log(state);
+  if (state === "normal") {
+    if (checkSupersaiyan()) {
+      drawSuperSaiyanHair();
+      drawSuperSaiyanAurora();
+    } else {
+      drawNormalHair();
+    }
+  }
+
+  if (state === "charging") {
+    powerLevel.updatePowerLevel(currentCharge);
+    if (checkSupersaiyan()) {
+      drawSuperSaiyanHair();
+    } else {
+      drawNormalHair();
+      translate(random(-5, 5), random(-5, 5));
+    }
+    drawSuperSaiyanAurora();
+    currentCharge = random(1000, 1000000);
+  }
+
+  drawPowerLevel();
   //   drawSuperSaiyanEyes();
-  //   drawNormalHair();
   // We can call both functions to draw all keypoints and the skeletons
   //   drawKeypoints();
   //   drawSkeleton();
+}
+
+function checkSupersaiyan() {
+  if (powerLevel) {
+    return powerLevel.isSuperSaiyan();
+  }
+  return false;
+}
+
+function drawPowerLevel() {
+  if (powerLevel) {
+    powerLevel.show();
+  }
 }
 
 // Detect the position of head and add hair
@@ -131,7 +293,6 @@ function drawNormalHair() {
     const {
       pose: { leftEar, rightEar, nose, rightEye, leftEye }
     } = poses[0];
-    console.log(poses[0]);
 
     if (leftEar && rightEar && nose && rightEye && leftEye) {
       const { x, y } = leftEar;
