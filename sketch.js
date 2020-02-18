50b9ff29f873febbf148d8b13acf818e22ccfc65
David <davidyu37@aliyun.com>
2020-02-18 15:43:50 +0800

    aurora art

--- sketch.js
+++ sketch.js
@@ -1,140 +1,11 @@
-let bodypix;
-let segmentation;
-
-const videoWidth = 640;
-const videoHeight = 480;
+const videoWidth = window.screen.width;
+const videoHeight = window.screen.height;
 
 let video;
 let poseNet;
 let poses = [];
 
-let auroras = [];
-let numAuroras = 40;
-
 let super_saiyan_hair, goku_hair, aurora;
-
-class Bug {
-  constructor(tempX, tempY, tempR) {
-    this.x = tempX;
-    this.y = tempY;
-    this.radius = tempR;
-
-    // pick a random color
-    this.color = color(255);
-    let r = random(3);
-    if (r < 1) {
-      this.color = color(255, 100, 20, 50); // orange
-    } else if (r >= 1 && r < 2) {
-      this.color = color(255, 200, 10, 50); // yellow
-    } else if (r >= 2) {
-      this.color = color(255, 80, 5, 50); // reddish
-    }
-  }
-
-  show() {
-    noStroke();
-    fill(this.color);
-    ellipse(this.x, this.y, this.radius);
-  }
-
-  move() {
-    this.x += random(-5, 5);
-    this.y -= random(1, 3);
-  }
-
-  shrink() {
-    // shrink size over time
-    this.radius -= 0.4;
-  }
-}
-
-class Aurora {
-  constructor(centerX, centerY, maxBoundary) {
-    this.centerX = centerX;
-    this.centerY = centerY;
-    this.maxBoundary = maxBoundary;
-  }
-
-  show() {
-    const { minX, minY, maxX, maxY } = this.maxBoundary;
-    console.log("show");
-
-    const height = maxY - minY;
-    // const width = maxX - minX;
-
-    translate(0, height / 2);
-
-    var a = -height / 3;
-    var b = width / 2;
-
-    noFill();
-    beginShape();
-    var x = 0;
-    while (x < width) {
-      let y = ((x - b) * (x - b)) / (maxX - minX) + a;
-      console.log(x, y);
-      vertex(x, y);
-      x = x + 10;
-
-      endShape();
-    }
-
-    translate(0, -height / 2);
-
-    // const width = maxX - minX;
-    // const height = maxY - minY;
-
-    // fill(255, 255, 0, 100);
-
-    // ellipse(this.centerX, this.centerY, width, height, 5);
-    // const startX = this.centerX;
-    // const startY = minY;
-
-    // noFill();
-
-    // let currentX = startX,
-    //   currentY = startY;
-    // beginShape();
-
-    // for (let index = 0; index < 50; index++) {
-    //   console.log(currentX, currentY);
-    //   curveVertex(currentX, currentY);
-
-    //   currentX = 0.3 * Math.pow(currentX, 2);
-
-    //   currentY = currentX ;
-    // }
-
-    // endShape();
-
-    // drawOneSide(startX, startY, false);
-    // drawOneSide(startX, startY, true);
-
-    // function drawOneSide(startX, startY, left) {
-    //   beginShape();
-    //   let currentX = startX,
-    //     currentY = startY;
-    //   for (let index = 0; index < 50; index++) {
-    //     console.log("draw", currentX, currentY);
-    //     curveVertex(currentX, currentY);
-
-    //     if (left) {
-    //       currentX -= 5;
-    //     } else {
-    //       currentX += 5;
-    //     }
-    //     currentY += 10;
-
-    //     if (index % 2 == 0) {
-    //       currentY -= 20;
-    //     } else {
-    //       currentY += 10 * 1.2;
-    //     }
-    //   }
-    //   endShape();
-    // }
-  }
-}
 
 function preload() {
   super_saiyan_hair = loadImage("assets/hair.png");
@@ -165,7 +36,7 @@ function modelReady() {
 function draw() {
   image(video, 0, 0, width, height);
   drawSuperSaiyanAurora();
-  //   drawSuperSaiyanHair();
+  drawSuperSaiyanHair();
   //   drawSuperSaiyanEyes();
   //   drawNormalHair();
   // We can call both functions to draw all keypoints and the skeletons
@@ -222,44 +93,16 @@ function drawSuperSaiyanAurora() {
     const width = maxX - minX;
     const height = maxY - minY;
 
-    const xMovedBy = width / 3;
-    const yMovedBy = height / 4;
+    const xMovedBy = width;
+    const yMovedBy = height / 2;
 
     const posX = minX - xMovedBy;
     const posY = minY - yMovedBy;
 
-    const paddedWidth = (width + xMovedBy) * 1.2;
-    const paddedHeight = (height + yMovedBy) * 1.3;
+    const paddedWidth = (width + xMovedBy) * 1.6;
+    const paddedHeight = (height + yMovedBy) * 1.5;
 
-    // stroke(255, 255, 0);
-    // noFill();
-    // strokeWeight();
-
-    // fill(255, 255, 0, 150);
-    // rect(posX, posY, paddedWidth, paddedHeight);
-
-    // for (let i = auroras.length - 1; i >= 0; i--) {
-    //   //   auroras[i].move();
-    //   //   auroras[i].shrink();
-    //   auroras[i].show();
-
-    //   //   if (auroras[i].radius <= 0) {
-    //   //     //remove the dead ones
-    //   //     auroras.splice(i, 1);
-    //   //   }
-    // }
-
-    // let x = 200;
-    // let y = 300;
-    // let radius = random(30, 50);
-    let b = new Aurora(posX + paddedWidth / 2, posY + paddedHeight / 2, {
-      minX: posX,
-      minY: posY,
-      maxX: posX + paddedWidth,
-      maxY: posY + paddedHeight
-    });
-    b.show();
-    // auroras.push(b);
+    image(aurora, posX, posY, paddedWidth, paddedHeight);
   }
 }
 
